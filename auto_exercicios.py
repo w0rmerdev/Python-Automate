96c3da9e66a76049d378f0e826d11ab744c2ac5b
Tomás Antunes <tomasbanntunes@gmail.com>
2020-10-27 19:01:51 +0000

    Criação do repo e criação do auto_exercicios.py
    o auto_exercicios cria uma pasta em que gera o numero de ficheiros necessario para a resolução dos exercicícos das fichas de P1

---
+++ auto_exercicios.py
@@ -0,0 +1,28 @@
+#Automatizar o processo de criação de pastas e ficheiros para resolução de exercicíos 
+
+import os
+
+diretoria = str(input("diretoria: "))
+
+nome_pasta = str(input("nome da pasta: "))
+
+diretoria_nova_pasta = '/home/tomas/' + diretoria + '/' + nome_pasta
+
+nome_ficheiro = str(input("nome do ficheiro, sem número ou terminação: "))
+
+numero_exercicios = int(input("num total de exercícios: "))
+
+terminacao = str(input("terminação da linguagem : "))
+
+os.mkdir(diretoria_nova_pasta)
+    
+for i in range(1, numero_exercicios + 1):
+
+    with open("/home/tomas/Desktop/uni/1ºAno/Semestre_Impar/P1/Exercicios/default.c") as f:
+       
+        lines = f.readlines()
+        lines = [l for l in lines]
+        
+        with open(diretoria_nova_pasta + '/' + nome_ficheiro + str(i) + "." + terminacao, "x") as f1:
+            
+            f1.writelines(lines)
